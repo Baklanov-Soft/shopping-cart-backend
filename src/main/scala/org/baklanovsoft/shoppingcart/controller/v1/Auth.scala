e08bf99eb7a5eaf5b2e6e0269ec70cead1ea784d
DenisNovac <ponydnk@gmail.com>
2023-02-26 17:21:47 +0400

    SCB#6 Checkout service and error recovery

--- src/main/scala/org/baklanovsoft/shoppingcart/controller/v1/Auth.scala
+++ src/main/scala/org/baklanovsoft/shoppingcart/controller/v1/Auth.scala
@@ -4,14 +4,15 @@ import cats.Functor
 import cats.implicits._
 import org.baklanovsoft.shoppingcart.model.user.{JwtToken, User}
 import org.baklanovsoft.shoppingcart.service.user.AuthService
+import sttp.model.StatusCode
 
 final case class Auth[F[_]: Functor](
     authService: AuthService[F]
 ) {
 
-  def auth(jwtToken: JwtToken): F[Either[Unit, User]] =
+  def auth(jwtToken: JwtToken): F[Either[(StatusCode, String), User]] =
     authService
       .findUser(jwtToken)
-      .map(_.fold(().asLeft[User])(_.asRight))
+      .map(_.fold((StatusCode.Forbidden -> "Forbidden").asLeft[User])(_.asRight))
 
 }
--- src/main/scala/org/baklanovsoft/shoppingcart/model/payment/Card.scala
+++ src/main/scala/org/baklanovsoft/shoppingcart/model/payment/Card.scala
@@ -1,4 +1,8 @@
 package org.baklanovsoft.shoppingcart.model.payment
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
+import org.baklanovsoft.shoppingcart.util.rest.RestCodecs
+import sttp.tapir.Schema
 
 case class Card(
     name: String,
@@ -6,3 +10,8 @@ case class Card(
     expiration: String,
     cvv: String
 )
+
+object Card extends RestCodecs {
+  implicit val codec: Codec[Card]   = deriveCodec
+  implicit val schema: Schema[Card] = Schema.derived
+}
