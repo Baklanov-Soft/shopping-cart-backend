e4878998337a9bb90d72ea6e841e441f98c432a1
DenisNovac <ponydnk@gmail.com>
2023-02-26 15:45:46 +0400

    SCB#6 Orders controller

--- src/main/scala/org/baklanovsoft/shoppingcart/model/payment/Order.scala
+++ src/main/scala/org/baklanovsoft/shoppingcart/model/payment/Order.scala
@@ -1,6 +1,10 @@
 package org.baklanovsoft.shoppingcart.model.payment
 
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
+import org.baklanovsoft.shoppingcart.util.rest.RestCodecs
 import squants.market.Money
+import sttp.tapir.Schema
 
 case class Order(
     id: OrderId,
@@ -8,3 +12,8 @@ case class Order(
     items: List[CartItem],
     total: Money
 )
+
+object Order extends RestCodecs {
+  implicit val codec: Codec[Order]   = deriveCodec
+  implicit val schema: Schema[Order] = Schema.derived
+}
