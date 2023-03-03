49f92854dbfbf4da3f4938dae767ef6b473185e5
DenisNovac <ponydnk@gmail.com>
2023-03-03 13:46:09 +0400

    SCB#13 Add check user routes and logic

--- src/main/scala/org/baklanovsoft/shoppingcart/controller/v1/user/UserController.scala
+++ src/main/scala/org/baklanovsoft/shoppingcart/controller/v1/user/UserController.scala
@@ -3,13 +3,16 @@ package org.baklanovsoft.shoppingcart.controller.v1.user
 import cats.implicits._
 import cats.Functor
 import org.baklanovsoft.shoppingcart.controller.v1.{Auth, Controller, Routes}
-import org.baklanovsoft.shoppingcart.model.user.{CreateUser, JwtToken, LoginUser}
+import org.baklanovsoft.shoppingcart.model.user.{CreateUser, JwtToken, LoginUser, Username}
 import org.baklanovsoft.shoppingcart.util.rest.RestCodecs
 import sttp.model.StatusCode
 import sttp.tapir._
 import sttp.tapir.json.circe.jsonBody
 
 final case class UserController[F[_]: Functor](auth: Auth[F]) extends Controller[F] {
+
+  private val check =
+    UserController.check.serverLogicSuccess(auth.check)
 
   private val login =
     UserController.login.serverLogic { loginUser =>
@@ -37,6 +40,7 @@ final case class UserController[F[_]: Functor](auth: Auth[F]) extends Controller
 
   override val routes = List(
     login,
+    check,
     logout,
     resister
   )
@@ -45,6 +49,15 @@ final case class UserController[F[_]: Functor](auth: Auth[F]) extends Controller
 object UserController extends RestCodecs {
   private val tag  = "Auth"
   private val base = Routes.base / "auth"
+
+  private val check =
+    endpoint.get
+      .in(base / "check")
+      .in(query[Username]("username"))
+      .out(statusCode)
+      .errorOut(statusCode(StatusCode.NotFound).description("No such user found by username"))
+      .tag(tag)
+      .summary("Check user exists")
 
   private val login =
     endpoint.post
--- src/main/scala/org/baklanovsoft/shoppingcart/service/user/AuthService.scala
+++ src/main/scala/org/baklanovsoft/shoppingcart/service/user/AuthService.scala
@@ -5,7 +5,7 @@ import org.baklanovsoft.shoppingcart.model.user._
 trait AuthService[F[_]] {
   def findUser(token: JwtToken): F[Option[User]]
   def newUser(username: Username, password: Password): F[JwtToken]
+  def check(username: Username): F[Boolean]
   def login(username: Username, password: Password): F[JwtToken]
   def logout(token: JwtToken, username: Username): F[Unit]
-
 }
