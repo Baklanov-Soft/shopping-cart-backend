4b3d592e65dc237452e21dc0d8efa58511f3dc2b
DenisNovac <ponydnk@gmail.com>
2023-03-08 14:54:46 +0400

    SCB#28 Users SQL

--- app/src/main/resources/migrations/V1__init.sql
+++ app/src/main/resources/migrations/V1__init.sql
@@ -2,10 +2,12 @@ CREATE TABLE brands (
     uuid UUID PRIMARY KEY,
     name VARCHAR UNIQUE NOT NULL
 );
+
 CREATE TABLE categories (
     uuid UUID PRIMARY KEY,
     name VARCHAR UNIQUE NOT NULL
 );
+
 CREATE TABLE items (
     uuid UUID PRIMARY KEY,
     name VARCHAR UNIQUE NOT NULL,
@@ -20,3 +22,23 @@ CREATE TABLE items (
       REFERENCES categories (uuid) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+CREATE TABLE users (
+    uuid UUID PRIMARY KEY,
+    username VARCHAR UNIQUE NOT NULL,
+    passwordHashed VARCHAR NOT NULL
+);
+
+CREATE TABLE salt (
+    user_uuid UUID PRIMARY KEY,
+    salt VARCHAR NOT NULL,
+    iterations INTEGER NOT NULL,
+    CONSTRAINT user_uuid_fk FOREIGN KEY (user_uuid) REFERENCES users (uuid)
+);
+
+CREATE TABLE roles (
+    uuid UUID PRIMARY KEY,
+    user_uuid UUID NOT NULL,
+    role VARCHAR NOT NULL
+)
+
